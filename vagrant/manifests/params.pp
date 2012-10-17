76c4f0be26733ace0ddf15c3853525e207eb2387
Craig Davis <craig@there4development.com>
2012-10-17 08:58:59 -0500

    Add vagrant config for local development

---
+++ vagrant/manifests/params.pp
@@ -0,0 +1,16 @@
+class params
+{
+
+    # Hostname of the virtualbox (make sure this URL points to 127.0.0.1 on your local dev system!)
+    $host = 'concrete5.dev'
+
+    # Original port (don't change)
+    $port = '80'
+
+    # Database names
+    $dbname = 'concrete5'
+    $dbuser = 'c5admin'
+    $dbpass = 'XfDN7QhajRW1Vj'
+    $dbfile = ''
+
+}
